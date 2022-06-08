94bb70caed835cb8656e9d14ca98a4b50529bc77
TrevorTress <trevor.tress@outlook.com>
2022-06-08 12:28:17 -0400

    reaction schema and thought model updated

--- models/Thought.js
+++ models/Thought.js
@@ -1,8 +1,54 @@
 const { Schema, model, Types } = require('mongoose')
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            // required
+            // 280 char max
+        },
+        username: {
+            type: String,
+            // required
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //get: createdAtVal => dateFormat(createdAtVal)
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false 
+    }
+)
+
 const ThoughtSchema = new Schema(
     {
-        
+        thoughtText: {
+            type: String,
+            // required
+            // between 1 and 280 chars
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            //get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            // required
+        },
+        reactions: {
+            // Array of nested documents created with the reactionSchema
+        }
     },
     {
         toJSON: {
